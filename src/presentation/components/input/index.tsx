6f775f41bd0826bf8757ced5dca3dfc571be193c
Glaucio Ricardo <glaucio.ricardo.jun@accenture.com>
2020-07-24 13:10:50 -0300

    feat: remove autofill on input

--- src/presentation/components/input/index.tsx
+++ src/presentation/components/input/index.tsx
@@ -4,11 +4,9 @@ import Styles from './styles.scss'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
-export const Input: React.FC<Props> = (props) => {
-  return (
-    <div className={Styles.inputWrap}>
-      <input{...props} />
-      <span className={Styles.status}>🔴</span>
-    </div>
-  )
-}
+export const Input: React.FC<Props> = (props) => (
+  <div className={Styles.inputWrap}>
+    <input {...props} autoComplete="off" />
+    <span className={Styles.status}>🔴</span>
+  </div>
+)
